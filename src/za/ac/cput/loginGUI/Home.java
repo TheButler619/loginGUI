c15b38b4a2813d58c1624370a39ae18a3e32826e
Keenan <219264228@mycput.ac.za>
2021-06-06 19:00:52 +0200

    Added new panel

--- src/za/ac/cput/loginGUI/Home.java
+++ src/za/ac/cput/loginGUI/Home.java
@@ -6,57 +6,49 @@ import java.awt.event.ActionListener;
 
 
 public class Home extends JFrame implements ActionListener {
-    private JPanel panelCenter, panelSouth;
+    private JPanel panelNorth, panelCenter, panelSouth;
     private JButton btnCalender, btnTask, btnNotification, btnProgress, btnLogOut;
+    private JLabel lblWelcome;
 
     public Home(){
         super("Home");
+        panelNorth = new JPanel();
         panelCenter = new JPanel();
-        //panelEast = new JPanel();
-        //panelWest = new JPanel();
         panelSouth = new JPanel();
         btnCalender = new JButton("View Calender");
         btnNotification = new JButton("View Notifications");
         btnTask = new JButton("View Tasks");
         btnProgress = new JButton("View Progress");
         btnLogOut = new JButton("Log Out");
+        lblWelcome = new JLabel("Welcome", SwingConstants.CENTER);
     }
     public void setGUI(){
+        panelNorth.setLayout(new GridLayout(1,1));
         panelCenter.setLayout(new GridLayout(2,2,3,3));
-        //panelEast.setLayout(new GridLayout(1,1,3,3));
-        //panelWest.setLayout(new GridLayout(4,1,3,3));
         panelSouth.setLayout(new GridLayout(1,1));
 
-        panelCenter.setBorder(BorderFactory.createEmptyBorder(100,90,100,90));
+        panelNorth.setBorder(BorderFactory.createEmptyBorder(20,0,0,0));
+        panelCenter.setBorder(BorderFactory.createEmptyBorder(50,90,100,90));
         panelSouth.setBorder(BorderFactory.createEmptyBorder(0,250,25,250));
 
         //setting the GUI Background Color
+        panelNorth.setBackground(new Color(255,205,108));
         panelCenter.setBackground(new Color(255,205,108));
         panelSouth.setBackground(new Color(255,205,108));
-        //panelWest.setBackground(new Color(255,205,108));
-        //panelEast.setBackground(new Color(255,205,108));
 
         btnTask.setPreferredSize(new Dimension(60, 40));
         btnNotification.setPreferredSize(new Dimension(60, 40));
         btnProgress.setPreferredSize(new Dimension(60, 40));
         btnCalender.setPreferredSize(new Dimension(60, 40));
 
-        //btnTask.setPreferredSize(new Dimension(100, 20));
-        //btnNotification.setPreferredSize(new Dimension(100, 20));
-        //btnProgress.setPreferredSize(new Dimension(100, 20));
-        //btnCalender.setPreferredSize(new Dimension(100, 20));
-
         btnLogOut.setPreferredSize(new Dimension(90, 40));
+
+        panelNorth.add(lblWelcome);
 
         panelCenter.add(btnCalender);
         panelCenter.add(btnTask);
         panelCenter.add(btnNotification);
         panelCenter.add(btnProgress);
-
-        //panelWest.add(btnCalender);
-        //panelWest.add(btnTask);
-        //panelWest.add(btnNotification);
-        //panelWest.add(btnProgress);
 
         panelSouth.add(btnLogOut);
 
@@ -66,10 +58,9 @@ public class Home extends JFrame implements ActionListener {
         btnTask.addActionListener(this);
         btnLogOut.addActionListener(this);
 
+        this.add(panelNorth, BorderLayout.NORTH);
         this.add(panelCenter, BorderLayout.CENTER);
         this.add(panelSouth, BorderLayout.SOUTH);
-        //this.add(panelWest, BorderLayout.WEST);
-        //this.add(panelEast, BorderLayout.EAST);
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.pack();
